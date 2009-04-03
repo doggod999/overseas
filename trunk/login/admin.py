c56f80d53802e35d0a6c046a46e73e13d52c646a
doggod999 <doggod999@ed6b0f64-1e08-11de-9909-f37ff7dbbc12>
2009-04-03 17:08:50 +0000

    登录功能初步

---
+++ trunk/login/admin.py
@@ -0,0 +1,5 @@
+from overseas.login.models import User
+
+from django.contrib import admin
+
+admin.site.register(User)
---
+++ trunk/login/models.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from django.db import models
+
+# 用户
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    true_name = models.CharField(max_length=10)
+    is_admin = models.BooleanField(default=False)
+    email = models.EmailField()
