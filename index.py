0c3435252301e00a7e6f8ac409f2a68371155595
doggod999 <doggod999@ed6b0f64-1e08-11de-9909-f37ff7dbbc12>
2009-04-03 12:43:14 +0000

    更换media目录为static

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def main(request):
-    return HttpResponse("Hi, Django!")
+    return render_to_response('index.html', { })
--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -12,6 +14,8 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^$', 'overseas.index.main'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_PATH}),
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
