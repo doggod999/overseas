8d9ba9cb47f50a3081841d3898c13ecd8c94509b
doggod999 <doggod999@ed6b0f64-1e08-11de-9909-f37ff7dbbc12>
2009-04-04 13:49:39 +0000

    增加数据库表

--- home/models.py
+++ home/models.py
@@ -1,3 +1,46 @@
+# coding=utf-8
 from django.db import models
 
-# Create your models here.
+# 用户
+class User(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    true_name = models.CharField(max_length=10)
+    is_admin = models.BooleanField(default=False)
+    email = models.EmailField()
+    
+#新闻
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    post_time = models.DateTimeField(auto_now_add=True)
+    last_modify_time = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='/static/upload', null=True)
+    
+#项目
+class Project(models.Model):
+    STATE_CHOICES = (('A', '关注中'),
+                     ('D', '进行中'),
+                     ('R', '重提交'),
+                     ('S', '成功'),
+                     ('P', '失败申诉中'),
+                     ('F', '失败'))
+    TYPE_CHOICES = (('A', 'API'),
+                     ('U', 'UI'),
+                     ('H', 'API+UI'))
+    
+    state = models.CharField(max_length=1, choices=STATE_CHOICES)
+    name = models.CharField(max_length=255)
+    source = models.ForeignKey('ProjectSource')
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES)
+    description = models.TextField(null=True)
+    fund = models.FloatField()
+    duty_person = models.ForeignKey('User')
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    
+    
+#项目来源
+class ProjectSource(models.Model):
+    name = models.CharField(max_length=255)
--- login/models.py
+++ login/models.py
@@ -1,10 +1,2 @@
 # coding=utf-8
 from django.db import models
-
-# 用户
-class User(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=20)
-    true_name = models.CharField(max_length=10)
-    is_admin = models.BooleanField(default=False)
-    email = models.EmailField()
