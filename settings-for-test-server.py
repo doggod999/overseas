d91f57569fbe149db60e85ec8e773d5681788ea7
doggod999 <doggod999@ed6b0f64-1e08-11de-9909-f37ff7dbbc12>
2009-04-05 08:34:58 +0000

    使用apache服务器

--- settings-for-test-server.py
+++ settings-for-test-server.py
@@ -35,9 +35,9 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = 'D:/workspace/overseas'
+MEDIA_ROOT = ''
 
-STATIC_PATH = 'D:/workspace/overseas/static'
+STATIC_PATH = './static'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
@@ -70,7 +70,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'D:/workspace/overseas/template',
+    './template',
 )
 
 INSTALLED_APPS = (
