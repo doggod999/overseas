63fc3e6b5eb299053812cb818defd7f61f165b30
doggod999 <doggod999@ed6b0f64-1e08-11de-9909-f37ff7dbbc12>
2009-04-08 05:43:14 +0000

    本周时间

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import datetime
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
@@ -11,10 +13,20 @@ from overseas.home.models import Project
 def main(request):
     user = request.session.get('user', None)
     news = News.objects.all().order_by('-id')[0:2]
+    
+    sunday = getsunday()
+    print sunday
+    saturday = getsaturday()
+    print saturday
+    att = []
     attention = Project.objects.filter(state='A').order_by('-id')
+    for a in attention:
+        if a.start_time > sunday:
+            att.append(a)
+    
     return render_to_response('home.html', {'user': user, 
                                             'news': news,
-                                            'attention': attention,
+                                            'attention': att,
                                             })
     
 
@@ -46,3 +58,17 @@ def news(request, n_id):
     
 def attention(request, p_id):
     pass
+
+#本周第一天，周日
+def getsunday():
+    today = datetime.datetime.now()
+    w = today.weekday()
+    sunday = today - datetime.timedelta(days=w+1)
+    return sunday
+
+#本周最后一天，周六
+def getsaturday():
+    today = datetime.datetime.now()
+    w = today.weekday()
+    saturday = today + datetime.timedelta(days=5-w)
+    return saturday
